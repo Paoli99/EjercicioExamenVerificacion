b374fbfd9f52cd3de292ca39095663511e980851
Paoli99 <31941327+Paoli99@users.noreply.github.com>
2022-02-22 08:00:20 -0400

    deleted ejercico examen

--- Ejercicio1/src/main/java/verificacionUsuario/VerificacionUsuariosUtil.java
+++
@@ -1,9 +0,0 @@
-package verificacionUsuario;
-
-public class VerificacionUsuariosUtil {
-    public VerificacionUsuariosUtil(){}
-
-    public String verificacionUsuarios(String user){
-        return "T";
-    }
-}
