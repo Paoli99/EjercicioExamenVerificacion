2d6fa0396f8858e45eb2eb154ed0c01bb66dd9c8
Paoli99 <31941327+Paoli99@users.noreply.github.com>
2022-02-22 08:11:18 -0400

    commit 1

--- Ejercicio1/src/main/java/ejercicioRecuperatorio/EstudianteBecaUtils.java
+++ Ejercicio1/src/main/java/ejercicioRecuperatorio/EstudianteBecaUtils.java
@@ -1,4 +1,12 @@
 package ejercicioRecuperatorio;
 
 public class EstudianteBecaUtils {
+  public static boolean aplicaBeca(int ci){
+        return true;
+    }
+
+    public String getNota(int ci){
+        return "";
+    }
+
 }
