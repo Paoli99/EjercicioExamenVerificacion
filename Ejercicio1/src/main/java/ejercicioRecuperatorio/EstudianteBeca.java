989f82dfde2fde9b0c8fe6ee15ac398cbf09aaef
Paoli99 <31941327+Paoli99@users.noreply.github.com>
2022-02-22 08:20:51 -0400

    commit 2

--- Ejercicio1/src/main/java/ejercicioRecuperatorio/EstudianteBeca.java
+++ Ejercicio1/src/main/java/ejercicioRecuperatorio/EstudianteBeca.java
@@ -1,4 +1,26 @@
 package ejercicioRecuperatorio;
 
+
+
 public class EstudianteBeca {
+    EstudianteBecaUtils estudianteBecaUtils;
+    //EstudianteHelper estudianteHelper;
+
+    public EstudianteBeca() { estudianteBecaUtils = new EstudianteBecaUtils();}
+    //public EstudianteBecaH() { estudianteHelper = new EstudianteHelper();}
+
+    public String estudianteBeca(int ci){
+        String msg = "";
+                if(estudianteBecaUtils.aplicaBeca(ci)){
+
+
+                    if(estudianteBecaUtils.getNota(ci) >= 90){
+
+                    }
+                }
+
+        return msg;
+
+    }
+
 }
