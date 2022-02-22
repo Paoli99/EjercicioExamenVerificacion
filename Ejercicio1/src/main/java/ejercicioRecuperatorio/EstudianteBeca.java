b264850118effdeb4329db28037ba8688a2bc3d5
Paoli99 <31941327+Paoli99@users.noreply.github.com>
2022-02-22 08:31:58 -0400

    commit 3

--- Ejercicio1/src/main/java/ejercicioRecuperatorio/EstudianteBeca.java
+++ Ejercicio1/src/main/java/ejercicioRecuperatorio/EstudianteBeca.java
@@ -7,17 +7,27 @@ public class EstudianteBeca {
     //EstudianteHelper estudianteHelper;
 
     public EstudianteBeca() { estudianteBecaUtils = new EstudianteBecaUtils();}
+
+    public EstudianteBeca(EstudianteBecaUtils objetoMockeado) { estudianteBecaUtils = objetoMockeado;}
+
+
+
     //public EstudianteBecaH() { estudianteHelper = new EstudianteHelper();}
 
     public String estudianteBeca(int ci){
         String msg = "";
-                if(estudianteBecaUtils.aplicaBeca(ci)){
+            if(estudianteBecaUtils.aplicaBeca(ci)){
 
-
-                    if(estudianteBecaUtils.getNota(ci) >= 90){
-
-                    }
+                if(estudianteBecaUtils.getNota(ci) >= 90){
+                    msg = "SI APLICA A BECA";
                 }
+                else{
+                    msg="NO APLICA A BECA POR PROMEDIO ACADEMICO";
+                }
+            }
+            else{
+                msg = "EL ESTUDIANTE NO CURSO AUN EL 60% DE LAS MATERIAS";
+            }
 
         return msg;
 
